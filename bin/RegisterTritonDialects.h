806d90567eaf884015eb6fb761a1529d553d0b87
Minjang Kim <minjang@meta.com>
2024-05-06 11:00:50 -0700

    [BACKEND][CPU] Create TritonCPU and conversion dialects (#3)

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -5,6 +5,7 @@
 #include "third_party/nvidia/include/Dialect/NVWS/IR/Dialect.h"
 #include "third_party/proton/dialect/include/Dialect/Proton/IR/Dialect.h"
 #include "triton/Dialect/Triton/IR/Dialect.h"
+#include "triton/Dialect/TritonCPU/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
 #include "triton/Dialect/TritonNvidiaGPU/IR/Dialect.h"
 
@@ -82,6 +83,7 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
 
   registry.insert<
       mlir::triton::TritonDialect, mlir::cf::ControlFlowDialect,
+      mlir::triton::cpu::TritonCPUDialect,
       mlir::triton::nvidia_gpu::TritonNvidiaGPUDialect,
       mlir::triton::gpu::TritonGPUDialect, mlir::math::MathDialect,
       mlir::arith::ArithDialect, mlir::scf::SCFDialect, mlir::gpu::GPUDialect,
